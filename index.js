f50ee9611b7034998cd270fae8cb7b7a7cdb64eb
Fernando Armonelli Fiedler <fernandoarmonelli@gmail.com>
2024-10-15 23:46:56 -0300

    install express

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hola Mundo!")
+const express = require( 'express' );
+const app = express();
+const port = 3000;
+
+app.get( '/', ( req, res ) => {
+  res.send("Hola mi server en express");
+} );
+
+app.listen(port, () => {
+  console.log("Mi port " +  port)
+});
